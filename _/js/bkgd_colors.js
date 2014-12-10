055eef27b36929fbd54f70bbfaa5a658070ad686
franknelson <fnelson12@gmail.com>
2014-12-09 21:42:49 -0600

    simplifying the css, html, js

--- _/js/bkgd_colors.js
+++ _/js/bkgd_colors.js
@@ -2,58 +2,63 @@
 	function getRandomInt(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 		}
-	
-	var winH = $(window).height();
-	
+
+	var h=window.innerHeight/5 + "px";
+
 var app = {
-		
+
 	colorMachine: function() {
 		var R = getRandomInt(0, 255);
 		var G = getRandomInt(0, 255);
 		var B = getRandomInt(0, 255);
-		var randomColor = (R + "," + G + "," + B + "," + 1);
+		var randomColor = (R + "," + G + "," + B + "," + 0.8);
 		var newColor = "rgba(" + randomColor + ")";
 		return newColor;
 	},
-	
+
 	timeMachine:function(){
 		var T = getRandomInt(30, 60);
 		var newT = (T+"00");
 		console.log(newT);
 		return newT;
 	},
-	
+
 	pageStyles: function(){
-		$("div #1 h1").html(app.colorMachine());
-			$("div #1").css({
-			'background-color': app.colorMachine()
-			//'height': winH
-			});
-		
-		$("div #2 h1").html(app.colorMachine());
-		$("div #2").css({
-			'background-color': app.colorMachine()
-			//'height': winH
-			});
-			
-		$("div #3 h1").html(app.colorMachine());
-		$("div #3").css({
-			'background-color': app.colorMachine()
-			//'height': winH
+		$("div#1 h1").html(app.colorMachine());
+			$("div#1").css({
+			'background-color': app.colorMachine(),
+			'height': h
 			});
 
-		$("div #4 h1").html(app.colorMachine());
-		$("div #4").css({
-			'background-color': app.colorMachine()
-			//'height': winH
+		$("div#2 h1").html(app.colorMachine());
+		$("div#2").css({
+			'background-color': app.colorMachine(),
+			'height': h
 			});
-		$("div #5 h1").html(app.colorMachine());
-		$("div #5").css({
-			'background-color': app.colorMachine()
-			//'height': winH
+
+		$("div#3 h1").html(app.colorMachine());
+		$("div#3").css({
+			'background-color': app.colorMachine(),
+			'height': h
+			});
+
+		$("div#4 h1").html(app.colorMachine());
+		$("div#4").css({
+			'background-color': app.colorMachine(),
+			'height': h
+			});
+
+		$("div#5 h1").html(app.colorMachine());
+		$("div#5").css({
+			'background-color': app.colorMachine(),
+			'height': h
+			});
+
+		$("div h1").css({
+			'line-height': h
 			});
 	},
-	
+
 	init: function() {
 		console.log("color 1: " + app.colorMachine());
 		console.log("color 2: " + app.colorMachine());
@@ -67,5 +72,6 @@ var app = {
 };
 
 $(document).ready(function() {
+	app.pageStyles();
 	app.init();
 });
