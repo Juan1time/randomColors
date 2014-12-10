4bda4f8a26e5a9c120d434dbf05b3bd3f8e8689f
franknelson <fnelson12@gmail.com>
2014-12-09 20:15:25 -0600

    initial commit

--- _/js/bc_f1.js
+++ _/js/bc_f1.js
@@ -2,7 +2,7 @@
 	function getRandomInt(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 		};
-	
+
 	function colorMachine(){
 		var R = getRandomInt(0, 255);
 		var G = getRandomInt(0, 255);
@@ -11,18 +11,18 @@
 		var newColor = "rgba(" + randomColor + ")";
 		return newColor;
 	};
-	
+
 	function timeMachine(){
 		var T = getRandomInt(30, 60);
 		var newT = (T+"00");
 		console.log(newT);
 		return newT;
 	};
-	
-	var winH = $(window).height();
-	
+
+	var winH = window.innerHeight + "px";
+
 var app = {
-		
+
 	/*colorMachine: function() {
 		var R = getRandomInt(0, 255);
 		var G = getRandomInt(0, 255);
@@ -30,28 +30,28 @@ var app = {
 		var randomColor = (R + "," + G + "," + B + "," + 1);
 		var newColor = "rgba(" + randomColor + ")";
 		return newColor;
-	}, 
-	
+	},
+
 	timeMachine:function(){
 		var T = getRandomInt(30, 60);
 		var newT = (T+"00");
 		console.log(newT);
 		return newT;
 	}, */
-	
+
 	pageStyles: function(){
 		$("div #1 h1").html(colorMachine);
 			$("div #1").css({
 			'background-color': colorMachine()
 			//'height': winH
 			});
-		
+
 		$("div #2 h1").html(colorMachine);
 		$("div #2").css({
 			'background-color': colorMachine()
 			//'height': winH
 			});
-			
+
 		$("div #3 h1").html(colorMachine);
 		$("div #3").css({
 			'background-color': colorMachine()
@@ -69,7 +69,7 @@ var app = {
 			//'height': winH
 			});
 	},
-	
+
 	init: function() {
 		/*console.log("color 1: " + app.colorMachine());
 		console.log("color 2: " + app.colorMachine());
