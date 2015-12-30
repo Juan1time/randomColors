a008998469584283da6bc97940511e6f46a69b5e
Frank Nelson <nels2371@umn.edu>
2015-12-30 11:17:19 -0600

    added buttons to control the background

--- _/js/bc_f1.js
+++ _/js/bc_f1.js
@@ -32,6 +32,34 @@ var app = {
 		return newColor;
 	},
 
+	rMonoRGB: function() {
+		var R = rInt(0, 255);
+		if (R<=50){
+			var mR=0;
+		} else{
+			var mR=R-50;
+		}
+		var G = rInt(0, 255);
+		if (G<=50){
+			var mG=0;
+		} else{
+			var mG=G-50;
+		}
+		var B = rInt(0, 255);
+		if (B=50){
+			var mB=0;
+		} else{
+			var mB=B-50;
+		}
+		var randomColor = (R + "," + G + "," + B + "," + 1);
+		var randomMColor = (mR + "," + mG + "," + mB + "," + 1);
+		var newColor = "rgba(" + randomColor + ")";
+		var newMColor = "rgba(" + randomMColor + ")";
+
+		//return newColor, newMColor;
+		return {newColor,newMColor};
+	},
+
 	rDelay:function(){
 		var T = rInt(30, 60);
 		var newT = (T+"00");
@@ -41,18 +69,84 @@ var app = {
 
 	rGradient: function(element){
 		$(element).css({
-			'background': 'linear-gradient(to top,'+rRGB() + ',' + rRGB()
+			'background': 'linear-gradient(to bottom,'+rRGB() + '15%,' + rRGB()+'85%'
+		})
+	},
+
+	rWhiteGradient: function(element){
+		$(element).css({
+			'background': 'linear-gradient(to bottom,'+rRGB() + '15%, rgba(255,255,255,1)'
+		})
+	},
+
+	rBlackGradient: function(element){
+		$(element).css({
+			'background': 'linear-gradient(to bottom,'+rRGB() + ', rgba(5,5,5,1)'
+		})
+	},
+
+	rMonoGradient: function(element){
+		var colors=app.rMonoRGB();
+		console.log(colors.newColor);
+		console.log(colors.newMColor);
+		$(element).css({
+			'background': 'linear-gradient(to bottom,'+colors.newColor + '15% ,' + colors.newMColor + '85%'
+		})
+	},
+
+	rSolid: function(element){
+		$(element).css({
+			'background': rRGB()
 		})
 	},
 
 	keyAction: function(){
 		$(window).keypress(function (e) {
-		  if (e.keyCode === 0 || e.keyCode === 32) {
+			console.log(e);
+		  if (e.keyCode === 32) {
 		    e.preventDefault()
-		    console.log('Space pressed');
+		    console.log('spacebar = background gradient');
 		    app.rGradient("body");
+		  } 
+		  	else if(e.keyCode === 118){
+		  	e.preventDefault()
+		    console.log('v = solid background');
+		    app.rSolid("body");
+		  }
+		  	else if (e.keyCode === 98){
+		  	e.preventDefault()
+		    console.log('b = gradient to white');
+		    app.rWhiteGradient("body");
+		  }
+		  else if (e.keyCode === 110){
+		  	e.preventDefault()
+		    console.log('n = gradient to black');
+		    app.rBlackGradient("body");
+		  }
+		  else if (e.keyCode === 109){
+		  	e.preventDefault()
+		    console.log('m = monochromatic gradient');
+		    app.rMonoGradient("body");
 		  }
 		})
+	},
+
+	buttons:function(){
+		$( "button#solid" ).click(function() {
+		  app.rSolid("body");
+		});
+		$( "button#gradient" ).click(function() {
+		  app.rGradient("body");
+		});
+		$( "button#gradient-black" ).click(function() {
+		  app.rBlackGradient("body");
+		});
+		$( "button#gradient-white" ).click(function() {
+		  app.rWhiteGradient("body");
+		});
+		$( "button#gradient-mono" ).click(function() {
+		  app.rMonoGradient("body");
+		});
 	},
 
 	pageStyles: function(){
@@ -94,14 +188,16 @@ var app = {
 		console.log("color 5: " + app.rRGB());*/
 		/*setInterval(app.pageStyles, app.rDelay());*/
 		//setInterval(app.pageStyles, rDelay());
-		setInterval(app.rGradient("body"), app.rDelay());
+		//setInterval(app.rGradient("body"), app.rDelay());
+		setInterval(app.rGradient("body"), 1000);
 		console.log("new ms: " + rDelay());
-		console.log("new ms: " + app.rGradient());
+		app.keyAction();
+		app.buttons();
+		console.log(app.rMonoRGB());
 	}
 
 };
 
 $(document).ready(function() {
 	app.init();
-	app.keyAction();
 });
